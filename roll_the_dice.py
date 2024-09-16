b3b51833a0ac6b5bdd8d389aee4fe3f25d1b0ab3
MiguelRomanR <rromanmiguelang@gmail.com>
2024-09-16 00:30:31 -0500

    Se termina el código completo

--- roll_the_dice.py
+++ roll_the_dice.py
@@ -1,1 +1,17 @@
 import random
+
+# Creamos una variable para que el no de usuario y otra
+# para que pueda ingresar su opción
+name = input("What's your name?: ")
+
+while True:
+    choice = input("\nRoll the dice? (y/n): ").lower()
+    if choice == 'y':
+        dice_1 = random.randint(1, 6)
+        dice_2 = random.randint(1, 6)
+        print(f'{dice_1}, {dice_2}')
+    elif choice == 'n':
+        print(f'Good bye {name}, Thanks for playing')
+        break
+    else:
+        print('Invalid choice')
