10279851de0b05796c83a9eead0abd3823dc2207
MiguelRomanR <rromanmiguelang@gmail.com>
2024-09-24 23:26:04 -0500

    Se actualiza el código y se ingresa las nuevas variables como, count, user, total

--- roll_the_dice.py
+++ roll_the_dice.py
@@ -1,17 +1,26 @@
-import random
+import random  # Asegúrate de importar el módulo random al inicio
 
-# Creamos una variable para que el no de usuario y otra
-# para que pueda ingresar su opción
-name = input("What's your name?: ")
+# Solicitar el nombre del usuario
+user = input("Enter your name: ").strip()
+print(f'\nHi {user}, Welcome to the game! Good luck!! 🙌')
 
 while True:
-    choice = input("\nRoll the dice? (y/n): ").lower()
-    if choice == 'y':
+    # Preguntar si el usuario quiere lanzar los dados
+    game = input(
+        f"\n{user}, do you want to roll the dice? (y/n): ").strip().lower()
+
+    # Validar la respuesta del usuario
+    if game == 'y':
+        # Lanzar los dados y calcular el total
         dice_1 = random.randint(1, 6)
         dice_2 = random.randint(1, 6)
-        print(f'{dice_1}, {dice_2}')
-    elif choice == 'n':
-        print(f'\nGood bye {name}, Thanks for playing')
+        total = dice_1 + dice_2
+        print(
+            f"\n  ➡️ You rolled {dice_1} and {dice_2}, that's {total} points!")
+
+    elif game == 'n':
+        print(f'\n  ➡️ Thanks for playing, come back later! 🙌\n')
         break
+
     else:
-        print('Invalid choice')
+        print(f'\nPlease enter a valid option (y/n).')
